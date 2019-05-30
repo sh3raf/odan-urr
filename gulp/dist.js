7252bdea24b4a75593791ecd11e954c02875ecfc
seraf <seraf@gmx.com>
2019-05-30 12:11:23 +0200

    add autoprefixer

--- gulp/dist.js
+++ gulp/dist.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 //compile
 gulp.task('sass', function () {
     return gulp.src('./app/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(gulp.dest('dist/css'));
 });
 
